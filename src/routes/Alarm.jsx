481bad4e095305e250c35ac078da308c748679b3
latapac <latashree@gmail.com>
2025-03-20 17:23:33 +0530

    ALARAM

--- src/routes/Alarm.jsx
+++ src/routes/Alarm.jsx
@@ -84,7 +84,7 @@ function AlarmReport() {
     function formatTimestamp(isoString) {
         console.log(isoString);
         
-        if (!isoString) return 'N/A';
+        if (!isoString) return ' ';
         const date = new Date(isoString);
         return `${date.getFullYear()}/${(date.getMonth() + 1).toString().padStart(2, '0')}/` +
             `${date.getDate().toString().padStart(2, '0')} ` +
